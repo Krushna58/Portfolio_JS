e7a42332e95b8bd754ebfb4da865206a339c7e1a
Krushna Bhosale <bhosalekrushna34@gmail.com>
2025-03-17 11:59:00 +0530

    contact me- ( added email send)

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-import emailjs from '@emailjs/browser'
 
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
+
+
     
-   function HandleSubmit() {
-     
-        
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
-
-        console.log(name);
-        
-        
-        if (!name || !email || !message) {
-            alert("Please fill in all required fields.");
-            return;
-        }
-        
-        // Send email using EmailJS
-        emailjs.send("service_sn3kz29", "template_zhr559o", {
-            from_name: name,
-            to_name: "Krushna Bhosale",
-            from_email: email,
-            message: message,
-        }, "FysKYelrvR0pjtzkF")
-        .then(() => {
-            alert("Your message has been sent successfully!");
-            form.reset();
-        })
-        .catch(error => {
-            console.error("EmailJS error:", error);
-            alert("Failed to send the email.");
-        });
-    });
-});
+   
